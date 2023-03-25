4f2ec7c7ad0c3a86eaf6d53155add87d3ffbd1f9
punjabactivism <128668419+punjabactivism@users.noreply.github.com>
2023-03-25 13:54:57 -0700

    Update cats.tsx

--- src/pages/cats.tsx
+++ src/pages/cats.tsx
@@ -1,67 +1,47 @@
+import { GetStaticProps } from "next";
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
 import TwitterCardMeta from "../components/meta/TwitterCardMeta";
-import { SocialList } from "../components/SocialList";
-import { TagContent } from "../lib/tags";
-import TagLink from "../components/TagLink";
+import PostList from "../components/PostList";
+import config from "../lib/config";
+import { countPosts, listPostContent, PostContent } from "../../lib/posts";
+import { listTags, TagContent } from "../lib/tags";
+import Head from "next/head";
 
 type Props = {
+  posts: PostContent[];
   tags: TagContent[];
+  pagination: {
+    current: number;
+    pages: number;
+  };
 };
-
-export default function Index({ tags }: Props) {
+export default function Index({ posts, tags, pagination }: Props) {
+  const url = "/posts";
+  const title = "All posts";
   return (
     <Layout>
-      <BasicMeta url={"/"} />
-      <OpenGraphMeta url={"/"} />
-      <TwitterCardMeta url={"/"} />
-      <div className="container">
-        <div>
-          <ul className={"categories"}>
-          </ul>
-        </div>
-      </div>
-      <style jsx>{`
-        .container {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex: 1 1 auto;
-          padding: 0 1.5rem;
-        }
-        h1 {
-          font-size: 2.5rem;
-          margin: 0;
-          font-weight: 500;
-        }
-        h2 {
-          font-size: 1.75rem;
-          font-weight: 400;
-          line-height: 1.25;
-        }
-        .fancy {
-          color: #15847d;
-        }
-        .handle {
-          display: inline-block;
-          margin-top: 0.275em;
-          color: #9b9b9b;
-          letter-spacing: 0.05em;
-        }
-
-        @media (min-width: 769px) {
-          h1 {
-            font-size: 3rem;
-          }
-          h2 {
-            font-size: 2.25rem;
-          }
-          .description {
-            font-size: 1.5rem;
-          }        
-        }
-      `}</style>
+      <BasicMeta url={url} title={title} />
+      <OpenGraphMeta url={url} title={title} />
+      <TwitterCardMeta url={url} title={title} />
+      <PostList posts={posts} tags={tags} pagination={pagination} />
     </Layout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = listPostContent(1, config.posts_per_page);
+  const tags = listTags();
+  const pagination = {
+    current: 1,
+    pages: Math.ceil(countPosts() / config.posts_per_page),
+  };
+  return {
+    props: {
+      posts,
+      tags,
+      pagination,
+    },
+  };
+};
