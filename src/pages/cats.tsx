f04f86bfb94af6daf1e332ab88f883c8b55886c5
punjabactivism <128668419+punjabactivism@users.noreply.github.com>
2023-03-25 13:13:33 -0700

    Update cats.tsx

--- src/pages/cats.tsx
+++ src/pages/cats.tsx
@@ -3,6 +3,7 @@ import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
 import TwitterCardMeta from "../components/meta/TwitterCardMeta";
 import { SocialList } from "../components/SocialList";
+import { TagContent } from "../lib/tags";
 
 export default function Index() {
   return (
