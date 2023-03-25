41f2bccf556b93faddd6c2a574a84080ddd1bd7b
punjabactivism <128668419+punjabactivism@users.noreply.github.com>
2023-03-24 21:38:47 -0700

    Create social.tsx

---
+++ src/pages/social.tsx
@@ -0,0 +1,65 @@
+import Layout from "../components/Layout";
+import BasicMeta from "../components/meta/BasicMeta";
+import OpenGraphMeta from "../components/meta/OpenGraphMeta";
+import TwitterCardMeta from "../components/meta/TwitterCardMeta";
+import { SocialList } from "../components/SocialList";
+
+export default function Index() {
+  return (
+    <Layout>
+      <BasicMeta url={"/"} />
+      <OpenGraphMeta url={"/"} />
+      <TwitterCardMeta url={"/"} />
+      <div className="container">
+        <div>
+          <h1>
+            We're concerned about the situation in Punjab.<span className="fancy">.</span>
+          </h1>
+          {/* <p className="description">PunjabActivism.com is a platform dedicated to raising awareness about the human rights abuses and political unrest in Punjab, India. We provide resources and tools for activists and advocates fighting against mass arrests, internet blackouts, and other forms of oppression in the region. Our mission is to amplify the voices of those who are fighting for justice and to support the movement for a more equitable and free Punjab.</p> */}
+          <span className="handle">@punjabactivism</span>
+          <SocialList />
+        </div>
+      </div>
+      <style jsx>{`
+        .container {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex: 1 1 auto;
+          padding: 0 1.5rem;
+        }
+        h1 {
+          font-size: 2.5rem;
+          margin: 0;
+          font-weight: 500;
+        }
+        h2 {
+          font-size: 1.75rem;
+          font-weight: 400;
+          line-height: 1.25;
+        }
+        .fancy {
+          color: #15847d;
+        }
+        .handle {
+          display: inline-block;
+          margin-top: 0.275em;
+          color: #9b9b9b;
+          letter-spacing: 0.05em;
+        }
+
+        @media (min-width: 769px) {
+          h1 {
+            font-size: 3rem;
+          }
+          h2 {
+            font-size: 2.25rem;
+          }
+          .description {
+            font-size: 1.5rem;
+          }        
+        }
+      `}</style>
+    </Layout>
+  );
+}
