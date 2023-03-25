7b90f74494747c7a7e9b75a7dcd2f9ad0f787a69
punjabactivism <128668419+punjabactivism@users.noreply.github.com>
2023-03-24 23:24:04 -0700

    Update PostItem.tsx

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -28,3 +28,17 @@ export default function PostItem({ post }: Props) {
     </Link>
   );
 }
+
+// Get all links on the page
+const links = document.getElementsByTagName('a');
+
+// Loop through each link
+for (let i = 0; i < links.length; i++) {
+  const link = links[i];
+
+  // Check if the link is an external link
+  if (link.host !== window.location.host) {
+    // If it is an external link, add the target="_blank" attribute
+    link.setAttribute('target', '_blank');
+  }
+}
